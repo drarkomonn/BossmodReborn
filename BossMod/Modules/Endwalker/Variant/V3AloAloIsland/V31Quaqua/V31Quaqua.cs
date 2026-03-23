382ab6d6929146006fb514d1450f40b81fa0c805
Drarko <drarko@devteam.com.ar>
2026-03-23 13:33:10 -0300

    alo alo first boss first attempt

---
+++ BossMod/Modules/Endwalker/Variant/V3AloAloIsland/V31Quaqua/V31Quaqua.cs
@@ -0,0 +1,56 @@
+namespace BossMod.Endwalker.VariantCriterion.V3AloAloIsland.V31QuaQua;
+
+public enum OID : uint
+{
+    Quaqua = 0x40BA,
+    Helper = 0x233C,
+    AethericCharge = 0x40BB,
+    ScaldingWaves = 0x40BE,
+    ScaldingWaves1 = 0x4134,
+}
+
+public enum AID : uint
+{
+    _Weaponskill_MadeMagic = 35732, // 40BA->self, 5.0s cast, range 50 circle
+    _Weaponskill_ArcaneArmaments = 35720, // 40BA->self, 8.0s cast, single-target
+    _Weaponskill_ = 35721, // 233C->self, no cast, single-target
+    _Weaponskill_RavagingAxe = 35722, // 40BB->self, 2.0s cast, range 14 circle
+    _Weaponskill_RingingQuoits = 35723, // 40BB->self, 2.0s cast, range 5-18 donut
+    _Weaponskill_ArcaneArmaments1 = 35724, // 40BA->self, 5.0s cast, single-target
+    _Weaponskill_HammerLanding = 35725, // 40BA->location, 8.0s cast, range 40 circle
+    _Weaponskill_HammerLanding1 = 35726, // 40BA->location, no cast, range 40 circle
+    _Weaponskill_VioletStorm = 35733, // 40BA->self, 5.5s cast, range 32 120.000-degree cone
+    _Weaponskill_Howl = 35734, // 40BA->self, 4.0s cast, single-target
+    _Weaponskill_ScaldingWaves = 35735, // 40BE->self, 5.0s cast, range 50 width 8 rect
+    _Weaponskill_ScaldingWaves1 = 35736, // 4134->self, no cast, range 50 width 4 rect
+}
+
+[SkipLocalsInit]
+sealed class QuaquaStates : StateMachineBuilder
+{
+    public QuaquaStates(BossModule module) : base(module)
+    {
+        TrivialPhase();
+    }
+}
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP,
+    StatesType = typeof(QuaquaStates),
+    ConfigType = null, // replace null with typeof(QuaquaConfig) if applicable
+    ObjectIDType = typeof(OID),
+    ActionIDType = null, // replace null with typeof(AID) if applicable
+    StatusIDType = null, // replace null with typeof(SID) if applicable
+    TetherIDType = null, // replace null with typeof(TetherID) if applicable
+    IconIDType = null, // replace null with typeof(IconID) if applicable
+    PrimaryActorOID = (uint)OID.Quaqua,
+    Contributors = "",
+    Expansion = BossModuleInfo.Expansion.Endwalker,
+    Category = BossModuleInfo.Category.VariantCriterion,
+    GroupType = BossModuleInfo.GroupType.CFC,
+    GroupID = 961u,
+    NameID = 12527u,
+    SortOrder = 1,
+    PlanLevel = 0)]
+[SkipLocalsInit]
+public sealed class Quaqua(WorldState ws, Actor primary) : BossModule(ws, primary, new(100f, 100f), new ArenaBoundsCircle(20f));
+
